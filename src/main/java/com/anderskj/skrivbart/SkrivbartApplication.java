9cd1098c87779f0f14090a6a8ff52630ce33ba04
Anders K <akj.code@gmail.com>
2023-04-28 00:45:00 +0200

    Added basic spring boot project...

---
+++ src/main/java/com/anderskj/skrivbart/SkrivbartApplication.java
@@ -0,0 +1,13 @@
+package com.anderskj.skrivbart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkrivbartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkrivbartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderskj/skrivbart/SkrivbartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderskj.skrivbart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkrivbartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
